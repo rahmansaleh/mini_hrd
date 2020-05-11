3d635a76966a7bc7556675dafef0ec69cc04fec9
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-05-11 22:09:13 +0700

    Update admin.py
    adding DivisiAdmin, KaryawanAdmin, AkunAdmin

--- karyawan/admin.py
+++ karyawan/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from karyawan.models import *
 
 # Register your models here.
+class DivisiAdmin (admin.ModelAdmin):
+    list_display = ['nama', 'keterangan']
+    list_filter = ()
+    search_fields = ['nama', 'keterangan']
+    list_per_page = 25
+
+admin.site.register(Divisi, DivisiAdmin)
+
+class KaryawanAdmin (admin.ModelAdmin):
+    list_display = ['nama', 'alamat', 'jenis_karyawan', 'jabatan', 'divisi', 'email', 'no_telpon']
+    list_filter = ('jenis_kelamin', 'jenis_karyawan', 'jabatan', 'divisi')
+    search_fields = ['nama', 'alamat', 'email', 'no_telpon']
+    list_per_page = 25
+
+admin.site.register(Karyawan, KaryawanAdmin)
+
+class AkunAdmin (admin.ModelAdmin):
+    list_display = ['akun', 'karyawan', 'jenis_akun']
+    list_filter = ['jenis_akun']
+    search_fields = []
+    list_per_page = 25
+
+admin.site.register(Akun, AkunAdmin)
