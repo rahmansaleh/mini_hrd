6dfa33da60c50b6af392c0ca29af50bdbc258f06
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2020-05-11 22:10:39 +0700

    Update models.py
    adding Kehadiran, libary future

--- kehadiran/models.py
+++ kehadiran/models.py
@@ -1,3 +1,19 @@
+from __future__ import unicode_literals
 from django.db import models
+from karyawan.models import Karyawan
 
 # Create your models here.
+class Kehadiran (models.Model):
+    JENIS_KEHADIRAN_CHOICES = (
+        ('izin', 'Izin'),
+        ('cuti', 'Cuti'),
+        ('alpha', 'Tanpa Alasan'),
+        ('hadir', 'Hadir'),
+    )
+
+    karyawan = models.ForeignKey(Karyawan, on_delete=models.PROTECT)
+    jenis_kehadiran = models.CharField(max_length=20, choices=JENIS_KEHADIRAN_CHOICES)
+    waktu = models.DateField()
+
+    def __unicode__(self):
+        return self.karyawan.nama
